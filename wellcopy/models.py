d73ac0f27940f280c76c0a292b6ca92d769dc8dd
lsanblas <leonardosanblas@gmail.com>
2023-11-20 18:52:46 +0100

    add entities
    mapping of entities of data model

--- wellcopy/models.py
+++ wellcopy/models.py
@@ -20,3 +20,80 @@ class Postcopy(models.Model) :
     def __str__(self):
         return self.title
 
+class Brand(models.Model) :
+    name = models.CharField(
+            max_length=50,
+            validators=[MinLengthValidator(2, "Name must be greater than 2 characters")]
+    )
+    description = models.TextField()
+    url = models.CharField(
+            max_length=50,
+            validators=[MinLengthValidator(2, "Url must be greater than 2 characters")]
+    )
+    
+    # Picture
+    picture = models.BinaryField(null=True, blank=True, editable=True)
+    content_type = models.CharField(max_length=256, null=True, blank=True, 
+                                    help_text='The MIMEType of the file')
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+            related_name='brand_owner')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Shows up in the admin list
+    def __str__(self):
+        return self.title
+    
+class Guitar(models.Model) :
+    name = models.CharField(
+            max_length=50,
+            validators=[MinLengthValidator(2, "Name must be greater than 2 characters")]
+    )
+    specs = models.TextField()
+    
+    # Picture
+    picture = models.BinaryField(null=True, blank=True, editable=True)
+    content_type = models.CharField(max_length=256, null=True, blank=True, 
+                                    help_text='The MIMEType of the file')
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+            related_name='guitar_owner')
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Shows up in the admin list
+    def __str__(self):
+        return self.title
+    
+class Review(models.Model) :
+    content = models.TextField()
+    score = models.FloatField()
+    likes = models.IntegerField()
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+            related_name='review_owner')
+    guitar = models.ForeignKey('Guitar', on_delete=models.CASCADE, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Shows up in the admin list
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model) :
+    content = models.TextField()
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+            related_name='review_owner')
+    review = models.ForeignKey('Review', on_delete=models.CASCADE, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Shows up in the admin list
+    def __str__(self):
+        return self.title
