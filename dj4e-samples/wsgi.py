7e879eac3e492893e57e6bd80f90a8d0d3e624b0
lsanblas <leonardosanblas@gmail.com>
2023-11-20 13:11:38 +0100

    revert wsgi.py
    reverted changes

--- dj4e-samples/wsgi.py
+++ dj4e-samples/wsgi.py
@@ -8,12 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-path = os.path.expanduser('~/dj4e-samples/dj4e-samples')
-if path not in sys.path:
-    sys.path.insert(0, path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'dj4e-samples.settings'
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "samples.settings")
+
+application = get_wsgi_application()
